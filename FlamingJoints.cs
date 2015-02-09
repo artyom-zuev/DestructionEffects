60e5938965ca0f459762f47c23415a28f0b11243
artyom-zuev <artyom.zuev@gmail.com>
2015-02-09 10:07:00 +0300

    Added a static bool to control logging across all classes, made the log message dependent on it, replaced all foreach loops with for loops

--- FlamingJoints.cs
+++ FlamingJoints.cs
@@ -7,64 +7,61 @@ namespace DestructionEffects
 	[KSPAddon(KSPAddon.Startup.Flight, false)]
 	public class FlamingJoints : MonoBehaviour
 	{
-		
 		public static List<GameObject> flameObjects = new List<GameObject>();
+        public static bool log = false;
 		
-		public void Start()
+		public void Start ()
 		{
 			GameEvents.onPartJointBreak.Add(onPartJointBreak);
-			
 		}
 		
-		public void onPartJointBreak(PartJoint partJoint)
+		public void onPartJointBreak (PartJoint partJoint)
 		{
-			
-			if(partJoint.Target!=null && partJoint.Target.PhysicsSignificance != 1)
+			if (partJoint.Target != null && partJoint.Target.PhysicsSignificance != 1)
 			{
 				Part part = partJoint.Target;
 				bool attachFlames = false;
-				
-				if(part.partInfo.title.Contains("Wing")
-					|| part.partInfo.title.Contains("Fuselage") 
-					|| part.FindModuleImplementing<ModuleEngines>() 
-					|| part.FindModuleImplementing<ModuleEnginesFX>()
-					)
+				if 
+                (
+                    part.partInfo.title.Contains("Wing") || 
+					part.partInfo.title.Contains("Fuselage") || 
+					part.FindModuleImplementing<ModuleEngines>() || 
+					part.FindModuleImplementing<ModuleEnginesFX>()
+                )
 				{
 					attachFlames = true;	
 				}
 				else
 				{
-					foreach(PartResource resource in part.Resources)
-					{
-						if(resource.resourceName.Contains("Fuel") || resource.resourceName.Contains("Ox"))	
-						{
-							attachFlames = true;	
-						}
-					}
+                    PartResourceList resources = part.Resources;
+                    for (int i = 0; i < resources.Count; ++i)
+                    {
+                        if (resources[i].resourceName.Contains ("Fuel") || resources[i].resourceName.Contains ("Ox"))
+                        {
+                            attachFlames = true;
+                        }
+                    }
 				}
-				if(attachFlames)
+				if (attachFlames)
 				{
 					GameObject flameObject2 = (GameObject) GameObject.Instantiate(GameDatabase.Instance.GetModel("DestructionEffects/Models/FlameEffect/model"), partJoint.transform.position, Quaternion.identity);
 					flameObject2.SetActive(true);
 					flameObject2.transform.parent = partJoint.Target.transform;
 					flameObject2.AddComponent<FlamingJointScript>();
-					foreach(var pe in flameObject2.GetComponentsInChildren<KSPParticleEmitter>())
+
+                    KSPParticleEmitter[] emitters = flameObject2.GetComponentsInChildren<KSPParticleEmitter> ();
+                    for (int i = 0; i < emitters.Length; ++i)
 					{
-						if(pe.useWorldSpace)	
+                        if (emitters[i].useWorldSpace)	
 						{
-							DEGaplessParticleEmitter gpe = pe.gameObject.AddComponent<DEGaplessParticleEmitter>();	
+                            DEGaplessParticleEmitter gpe = emitters[i].gameObject.AddComponent<DEGaplessParticleEmitter> ();	
 							gpe.part = partJoint.Target;
 							gpe.emit = true;
-							
 						}
 					}
 				}
 			}
-			
-			
-		}
-		
-		
+    	}
 	}
 }
 
