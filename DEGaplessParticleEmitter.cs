1be7513c183bc9c828df0d6f470fa99cb127661c
artyom-zuev <artyom.zuev@gmail.com>
2015-02-09 10:08:14 +0300

    Made the log message optional, replaced foreach loop with a for loop

--- DEGaplessParticleEmitter.cs
+++ DEGaplessParticleEmitter.cs
@@ -6,70 +6,59 @@ namespace DestructionEffects
 	public class DEGaplessParticleEmitter : MonoBehaviour
 	{
 		public KSPParticleEmitter pEmitter;	
-		
 		public float maxDistance = 1.1f;
-		
 		public bool emit = false;
-		
 		public Part part = null;
-		
 		public Rigidbody rb;
 		
-		
-		
-		void Start()
+		private void Start ()
 		{
 			pEmitter = gameObject.GetComponent<KSPParticleEmitter>();	
 			pEmitter.emit = false;
-			
-			
-			if(part!=null)
+			if (part != null)
 			{
-				Debug.Log ("Part "+part.partName+"'s explosionPotential: "+ part.explosionPotential);	
+                if (FlamingJoints.log) 
+                    Debug.Log ("DE | DEGaplessParticleEmitter | Start () | Part " + part.partName + " explosionPotential: " + part.explosionPotential);	
 			}
-			
-			maxDistance = pEmitter.minSize/3;
-		
-			
+			maxDistance = pEmitter.minSize / 3f;
 		}
-		
-		void FixedUpdate()
+
+        private void FixedUpdate ()
 		{
-			if(emit)
+			if (emit)
 			{
 				Vector3 velocity = (part == null) ? rb.velocity : part.rigidbody.velocity;
 				Vector3 originalLocalPosition = gameObject.transform.localPosition;
 				Vector3 originalPosition = gameObject.transform.position;
 				Vector3 startPosition = gameObject.transform.position + (velocity * Time.fixedDeltaTime);
-				float originalGapDistance = Vector3.Distance(originalPosition, startPosition);
+				float originalGapDistance = Vector3.Distance (originalPosition, startPosition);
 				float intermediateSteps = originalGapDistance/maxDistance;
-				
-				pEmitter.EmitParticle();
-				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, startPosition, maxDistance);
-				for(int i = 1; i < intermediateSteps; i++)
+
+				pEmitter.EmitParticle ();
+				gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, startPosition, maxDistance);
+				for (int i = 1; i < intermediateSteps; i++)
 				{
 					pEmitter.EmitParticle();
-					gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, startPosition, maxDistance);
+					gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, startPosition, maxDistance);
 				}
 				gameObject.transform.localPosition = originalLocalPosition;
 			}
 			
 		}
 		
-		public void EmitParticles()
+		public void EmitParticles ()
 		{
 			Vector3 velocity = (part == null) ? rb.velocity : part.rigidbody.velocity;
 			Vector3 originalLocalPosition = gameObject.transform.localPosition;
 			Vector3 originalPosition = gameObject.transform.position;
 			Vector3 startPosition = gameObject.transform.position + (velocity * Time.fixedDeltaTime);
-			float originalGapDistance = Vector3.Distance(originalPosition, startPosition);
+
+			float originalGapDistance = Vector3.Distance (originalPosition, startPosition);
 			float intermediateSteps = originalGapDistance/maxDistance;
-			
-			//gameObject.transform.position = startPosition;
-			for(int i = 0; i < intermediateSteps; i++)
+			for (int i = 0; i < intermediateSteps; i++)
 			{
 				pEmitter.EmitParticle();
-				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, startPosition, maxDistance);
+				gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, startPosition, maxDistance);
 			}
 			gameObject.transform.localPosition = originalLocalPosition;
 		}
